5bf03cde95762e1b4838a7d32da87da4ca9028e6
kkomilfo <a.nactia1978@gmail.com>
2024-03-19 02:21:47 +0100

    create basic components

---
+++ src/components/StudentsList.vue
@@ -0,0 +1,97 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {useStudentsStore} from "@/store/students.js";
+
+let studentsStore = useStudentsStore();
+
+onMounted(studentsStore.fetchStudents);
+
+const open = ref(false);
+
+const showModal = () => {
+	open.value = true;
+};
+
+const handleOk = async () => {
+	await studentsStore.createStudent();
+	open.value = false;
+};
+</script>
+
+<template>
+	<a-modal v-model:open="open" :confirm-loading="studentsStore.isLoading" title="Create a student" @ok="handleOk">
+		<a-form
+				:label-col="{ span: 24 }"
+				:model="studentsStore.student"
+				:wrapper-col="{ span: 24 }"
+				autocomplete="off"
+				layout="vertical"
+				name="basic"
+		>
+			<a-form-item
+					:rules="[{ required: true, message: 'Please input student full name!' }]"
+					label="Full Name"
+					name="full_name"
+			>
+				<a-input v-model:value="studentsStore.student.full_name"/>
+			</a-form-item>
+
+			<a-form-item
+					:rules="[{ required: true, message: 'Please input student password!' }]"
+					label="Password"
+					name="password"
+			>
+				<a-input-password v-model:value="studentsStore.student.password"/>
+			</a-form-item>
+
+			<a-form-item
+					:rules="[{ required: true, message: 'Please input avatar url!' }]"
+					label="Avatar URL"
+					name="avatar_url"
+			>
+				<a-input v-model:value="studentsStore.student.avatar_url"/>
+			</a-form-item>
+
+			<a-form-item
+					:rules="[{ required: true, message: 'Please input student email!' }]"
+					label="Email"
+					name="email"
+			>
+				<a-input v-model:value="studentsStore.student.email"/>
+			</a-form-item>
+		</a-form>
+	</a-modal>
+
+	<a-list
+			:data-source="studentsStore.students"
+			:loading="studentsStore.isLoading"
+			class="demo-loadmore-list"
+			item-layout="horizontal"
+			bordered
+	>
+		<template #header>
+			<a-flex justify="space-between" align="center">
+				<h2>Student List</h2>
+				<a-button type="primary" @click="showModal">Create Student</a-button>
+			</a-flex>
+		</template>
+		<template #renderItem="{ item }">
+			<a-list-item>
+				<a-list-item-meta
+						:description="item.email"
+				>
+					<template #title>
+						<a href="https://www.antdv.com/">{{ item.full_name }}</a>
+					</template>
+					<template #avatar>
+						<a-avatar :src="item.avatar_url"/>
+					</template>
+				</a-list-item-meta>
+			</a-list-item>
+		</template>
+	</a-list>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,120 +1,68 @@
 <script setup>
-import {onMounted} from 'vue';
-import {useStudentsStore} from "@/store/students.js";
+import {onMounted, ref} from 'vue';
 import {useTeachersStore} from "@/store/teachers.js";
+import StudentsList from "@/components/StudentsList.vue";
 
-let studentsStore = useStudentsStore();
 let teachersStore = useTeachersStore();
 
-onMounted(studentsStore.fetchStudents);
 onMounted(teachersStore.fetchTeachers);
 
 function isoToYYYYMMDD(isoString) {
 	return isoString.slice(0, 10).replace(/-/g, "-");
 }
+
+const headerStyle = {
+	border: '2px solid rgb(235, 237, 240)',
+	marginBottom: '10px',
+};
 </script>
 
 <template>
-	<a-row>
-		<a-col :span="12">
-			<a-form
-					:label-col="{ span: 4 }"
-					:model="studentsStore.student"
-					:wrapper-col="{ span: 24 }"
-					autocomplete="off"
-					layout="vertical"
-					name="basic"
-					@finish="studentsStore.createStudent"
-			>
-				<a-form-item
-						:rules="[{ required: true, message: 'Please input student full name!' }]"
-						label="Full Name"
-						name="full_name"
-				>
-					<a-input v-model:value="studentsStore.student.full_name"/>
-				</a-form-item>
-
-				<a-form-item
-						:rules="[{ required: true, message: 'Please input student password!' }]"
-						label="Password"
-						name="password"
-				>
-					<a-input-password v-model:value="studentsStore.student.password"/>
-				</a-form-item>
-
-				<a-form-item
-						:rules="[{ required: true, message: 'Please input avatar url!' }]"
-						label="Avatar URL"
-						name="avatar_url"
-				>
-					<a-input-password v-model:value="studentsStore.student.avatar_url"/>
-				</a-form-item>
-
-				<a-form-item
-						:rules="[{ required: true, message: 'Please input student email!' }]"
-						label="Email"
-						name="email"
-				>
-					<a-input-password v-model:value="studentsStore.student.email"/>
-				</a-form-item>
-
-				<a-form-item>
-					<a-button :disabled="studentsStore.isLoading" html-type="submit" type="primary">Submit</a-button>
-				</a-form-item>
-			</a-form>
-
-			<a-list
-					:data-source="studentsStore.students"
-					:loading="studentsStore.isLoading"
-					class="demo-loadmore-list"
-					item-layout="horizontal"
-			>
-				<template #renderItem="{ item }">
-					<a-list-item>
-						<a-list-item-meta
-								:description="item.email"
-						>
-							<template #title>
-								<a href="https://www.antdv.com/">{{ item.full_name }}</a>
-							</template>
-							<template #avatar>
-								<a-avatar :src="item.avatar_url"/>
-							</template>
-						</a-list-item-meta>
-					</a-list-item>
-				</template>
-			</a-list>
-		</a-col>
-		<a-col :span="12">
-			<a-list :data-source="teachersStore.teachers" item-layout="vertical" size="middle">
-				<template #renderItem="{ item }">
-					<a-list-item key="item.title">
-						<template #actions>
-							<span>id: {{ item.id }}</span>
-							<span>Date of birthday: {{ isoToYYYYMMDD(item.date_of_birth) }}</span>
-
+	<a-layout>
+		<a-page-header
+				:style="headerStyle"
+				title="Course Platform"
+				sub-title="Innovative online platform for university students and teachers"
+		/>
+		<a-layout-content style="margin: 5px">
+			<a-row :gutter="16">
+				<a-col :span="10">
+					<StudentsList />
+				</a-col>
+				<a-col :span="14">
+					<a-list bordered :data-source="teachersStore.teachers" item-layout="vertical" size="middle">
+						<template #header>
+							<h2>Teacher List</h2>
 						</template>
-						<a-list-item-meta :description="item.description">
-							<template #title>
-								<a>{{ item.full_name }}</a>
-							</template>
-							<template #avatar>
-								<a-avatar :src="item.avatar_url" size="large"/>
-							</template>
-						</a-list-item-meta>
-						<p>{{ item.education }}</p>
+						<template #renderItem="{ item }">
+							<a-list-item key="item.title">
+								<template #actions>
+									<span>id: {{ item.id }}</span>
+									<span>Date of birthday: {{ isoToYYYYMMDD(item.date_of_birth) }}</span>
+									a
+								</template>
+								<a-list-item-meta :description="item.description">
+									<template #title>
+										<a>{{ item.full_name }}</a>
+									</template>
+									<template #avatar>
+										<a-avatar :src="item.avatar_url" size="large"/>
+									</template>
+								</a-list-item-meta>
+								<p>{{ item.education }}</p>
 
-						<span v-for="{ title } in item.courses" :key="title">
+								<span v-for="{ title } in item.courses" :key="title">
 							<a-tag color="blue">{{ title }}</a-tag>
 						</span>
 
-					</a-list-item>
-				</template>
-			</a-list>
+							</a-list-item>
+						</template>
+					</a-list>
 
-		</a-col>
-	</a-row>
-
+				</a-col>
+			</a-row>
+		</a-layout-content>
+	</a-layout>
 
 </template>
 
